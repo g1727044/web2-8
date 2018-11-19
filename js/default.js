44f25f3f6aec0f2d1a10951209ac8189bca3e920
g1727044 <32856056+g1727044@users.noreply.github.com>
2018-11-19 17:22:42 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -37,3 +37,22 @@ for(key in player){
   li.textContent = key + ': ' + player[key];
   document.getElementById('object').appendChild(li);
 }
+var separate_time = function(time){
+  var sec   = time.getSeconds();
+  var min   = time.getMinutes();
+  var hours = time.getHours();
+  var days  = time.getdate();
+  var month = time.getMonth();
+  var year  = time.getFullYear();
+  return [sec, min, hours, days, month, year];
+}
+
+var now = new Date();
+var counter = separate_time(now);
+document.getElementById('countdown').textContent =
+  counter[5] + '年' +
+  counter[4] + '月' +
+  counter[3] + '日' +
+  counter[2] + '時' +
+  counter[1] + '分' +
+  counter[0] + '秒';
