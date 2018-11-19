b4c52cad71853779e9393810f8c1ffb376e9883f
g1727044 <32856056+g1727044@users.noreply.github.com>
2018-11-19 17:38:18 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,6 +1,3 @@
-var li = document.createElement('li');
-li.textContent = 'アイテム';
-document.getElementById('list').addChild(li);
 for(var i = 1; i < 10; i++){
   var li = document.createElement('li');
   li.textContent = i + '回目';
