87d7515f0f4e59008165fa66ed6d21a6506c5652
g1727044 <32856056+g1727044@users.noreply.github.com>
2018-11-19 17:28:22 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -41,7 +41,7 @@ var separate_time = function(time){
   var sec   = time.getSeconds();
   var min   = time.getMinutes();
   var hours = time.getHours();
-  var days  = time.getdate();
+  var days  = time.getDate();
   var month = time.getMonth();
   var year  = time.getFullYear();
   return [sec, min, hours, days, month, year];
